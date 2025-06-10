7b2aa4bdd119187cfa23485229388f46d5f49ea9
msgoodgirl92 <desanka.vajovic@gmail.com>
2025-06-10 16:25:48 +0200

    Delete portfolio directory

--- portfolio/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // Dodajte ovde produkcijske API URL-ove
-  apiUrl: 'https://vas-api-url.com' // promenite ovo na vaš produkcijski URL
-};
--- portfolio/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  // Dodajte ovde sve API URL-ove i druge konfiguracije
-  apiUrl: 'http://localhost:3000' // ili koji god URL koristite za development
-};
